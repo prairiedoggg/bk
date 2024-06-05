1e65e0499f8ba6186518a94ae33cc7cd774208f4
[AI10] 나경윤 <skruddbs99@naver.com>
2024-06-05 14:02:19 +0900

    Init: 프론트 초기 환경 세팅

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
