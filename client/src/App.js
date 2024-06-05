e9e343b3693548f9e007d1754df53ee5ac9c1f21
YiJeongan <wjddks110@kookmin.ac.kr>
2024-06-05 15:10:44 +0900

    Feat : 네브바 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
+        <Navbar></Navbar>
         <Routes>
-          <Route path="/login" element={<Login />} />
+          <Route path='/login' element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
