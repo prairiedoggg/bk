f44701dfb5eb9956908eca1a4a25975473b87372
YiJeongan <wjddks110@kookmin.ac.kr>
2024-06-05 16:45:37 +0900

    Feat : 소개 페이지 추가, Navbar 수정

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Navbar from "./components/common/Navbar";
+import Intro from "./pages/Intro";
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <Navbar></Navbar>
+        <Routes>
+          <Route path='/' element={<Intro />} />
+        </Routes>
         <Routes>
           <Route path='/login' element={<Login />} />
         </Routes>
