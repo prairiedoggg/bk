8aeea79de14bfff7fa9590948046f0f5db23e3ac
YiJeongan <wjddks110@kookmin.ac.kr>
2024-06-06 15:14:46 +0900

    Feat : 메인페이지 추가

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -3,60 +3,6 @@ import styled, { createGlobalStyle } from 'styled-components';
 import { Link } from 'react-router-dom';
 import { ReactComponent as LogoSVG } from '../../assets/icons/Logo.svg';
 
-const GlobalStyle = createGlobalStyle`
-  body, html {
-    margin: 0;
-    padding: 0;
-  }
-`;
-
-const NavbarWrapper = styled.div`
-  width: 100%; /* 네브바가 전체 너비를 차지하도록 설정 */
-  box-sizing: border-box; /* 패딩과 테두리를 포함한 너비 계산 */
-  border-bottom: 1px solid #c4c4c4; /* 더 얇은 선 */
-  margin: 0; /* 모든 마진 제거 */
-  padding: 10px 0; /* 모든 패딩 제거 */
-`;
-
-const NavbarContainer = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 10px 20px; /* 원하는 패딩 값을 설정 */
-  max-width: 1200px; /* 중앙 정렬을 위한 최대 너비 설정 */
-  margin: 0 auto; /* 중앙 정렬 */
-`;
-
-const LogoContainer = styled(Link)`
-  margin-right: auto;
-`;
-
-const StyledLogo = styled(LogoSVG)`
-  width: 210px;
-  height: 58.48px;
-`;
-
-const NavButton = styled.button`
-  font-family: 'SUITE';
-  font-style: normal;
-  font-weight: ${({ isActive }) => (isActive ? '700' : '400')};
-  font-size: 18px;
-  line-height: 16px;
-  text-align: center;
-  color: #543d20;
-  border: none;
-  background-color: transparent;
-  margin: 0 15px;
-  cursor: pointer;
-
-  &:hover {
-    font-weight: 700;
-  }
-`;
-
-const ButtonContainer = styled.div`
-  display: flex;
-`;
-
 function Button({ children, isActive, onClick }) {
   return (
     <NavButton isActive={isActive} onClick={onClick}>
@@ -123,5 +69,59 @@ function Navbar() {
     </>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  body, html {
+    margin: 0;
+    padding: 0;
+  }
+`;
+
+const NavbarWrapper = styled.div`
+  width: 100%; /* 네브바가 전체 너비를 차지하도록 설정 */
+  box-sizing: border-box; /* 패딩과 테두리를 포함한 너비 계산 */
+  border-bottom: 1px solid #c4c4c4; /* 더 얇은 선 */
+  margin: 0; /* 모든 마진 제거 */
+  padding: 10px 0; /* 모든 패딩 제거 */
+`;
+
+const NavbarContainer = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px 20px; /* 원하는 패딩 값을 설정 */
+  max-width: 1200px; /* 중앙 정렬을 위한 최대 너비 설정 */
+  margin: 0 auto; /* 중앙 정렬 */
+`;
+
+const LogoContainer = styled(Link)`
+  margin-right: auto;
+`;
+
+const StyledLogo = styled(LogoSVG)`
+  width: 210px;
+  height: 58.48px;
+`;
+
+const NavButton = styled.button`
+  font-family: 'SUITE';
+  font-style: normal;
+  font-weight: ${({ isActive }) => (isActive ? '700' : '400')};
+  font-size: 18px;
+  line-height: 16px;
+  text-align: center;
+  color: #543d20;
+  border: none;
+  background-color: transparent;
+  margin: 0 15px;
+  cursor: pointer;
+
+  &:hover {
+    font-weight: 700;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
 
 export default Navbar;
