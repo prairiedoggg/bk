770c60b2c6504006be6f02c75387f7d238221a7f
YiJeongan <wjddks110@kookmin.ac.kr>
2024-06-10 15:12:54 +0900

    Feat : 메인 모달창 리뷰작성 컴포넌트

--- client/src/components/main/ModalReviewContainer.js
+++ client/src/components/main/ModalReviewContainer.js
@@ -2,50 +2,42 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import ReviewStar from './ReviewStar';
 import WriteReviewIcon from '../../assets/icons/WriteReviewIcon.svg';
-import BackIcon from '../../assets/icons/BackIcon.svg'; // BackIcon을 가져옵니다.
-import ModalReviewWriteContainer from '../main/ModalReviewWriteContainer'; // ModalReviewWriteContainer를 가져옵니다.
+import BackIcon from '../../assets/icons/BackIcon.svg';
+import ModalReviewWriteContainer from '../main/ModalReviewWriteContainer';
 import ModalReviewListContainer from './ModalReviewListContainer';
 
 const ModalReviewContainer = ({ rating }) => {
-  // 상태를 사용하여 WriteReviewIcon 클릭 여부를 추적합니다.
   const [isWriteReviewOpen, setIsWriteReviewOpen] = useState(false);
-  // 아이콘 이미지를 바꾸기 위한 상태 추가
   const [iconImage, setIconImage] = useState(WriteReviewIcon);
 
-  // WriteReviewIcon을 클릭할 때 상태를 변경하여 모달을 표시합니다.
   const handleWriteReviewClick = () => {
     setIsWriteReviewOpen(true);
-    // 아이콘 이미지를 BackIcon으로 변경
     setIconImage(BackIcon);
   };
 
-  // BackIcon을 클릭할 때 다시 WriteReviewIcon으로 변경합니다.
   const handleBackIconClick = () => {
     setIsWriteReviewOpen(false);
-    // 아이콘 이미지를 WriteReviewIcon으로 변경
     setIconImage(WriteReviewIcon);
   };
 
   return (
     <Container>
       <ReviewTitle>
-        <span>리뷰(1)</span>
-        <span>
-          <ReviewStar rating={rating} />
-        </span>
-
-        {/* WriteReviewIcon 클릭 이벤트를 처리합니다. */}
+        <TitleText>
+          <span>리뷰(1)</span>
+          <span>
+            <ReviewStar rating={rating} />
+          </span>
+        </TitleText>
         <WriteReviewIconWrapper
           onClick={
             isWriteReviewOpen ? handleBackIconClick : handleWriteReviewClick
           }
         >
-          <img src={iconImage} alt='WriteReviewIcon' />{' '}
-          {/* 아이콘 이미지 변경 */}
+          <img src={iconImage} alt='WriteReviewIcon' />
         </WriteReviewIconWrapper>
       </ReviewTitle>
 
-      {/* isWriteReviewOpen이 true인 경우 ModalReviewWriteContainer를 표시합니다. */}
       {isWriteReviewOpen ? (
         <ModalReviewWriteContainer />
       ) : (
@@ -61,6 +53,7 @@ const Container = styled.div`
   top: 50%;
   padding: 20px 0;
   border-radius: 0 0 20px 20px;
+  width: 100%;
 `;
 
 const ReviewTitle = styled.div`
@@ -78,13 +71,20 @@ const ReviewTitle = styled.div`
   border-bottom: 0.5px solid #c0c0c0;
   box-sizing: border-box;
   padding-bottom: 5px;
+`;
+
+const TitleText = styled.div`
+  display: flex;
+  align-items: center;
   & > span {
     margin-right: 10px;
   }
+  flex-grow: 1;
 `;
 
 const WriteReviewIconWrapper = styled.span`
   display: flex;
   align-items: center;
-  margin-left: auto;
+  cursor: pointer;
+  margin-right: 0;
 `;
