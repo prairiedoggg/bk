da2e95a23df0e966bc16de0c3ff04caaa2a51576
[AI10] 나경윤 <skruddbs99@naver.com>
2024-06-15 00:33:06 +0900

    feat: 404페이지 추가

--- server/models/librarySchema.js
+++ server/models/librarySchema.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // mongoose 라이브러리에선 Number 타입이 소수점 숫자까지 포함하기 때문에 type을 굳이 float으로 바꿀 필요는 없음
@@ -12,7 +12,7 @@ const librarySchema = new Schema({
   holidays: String,
   latitude: Number,
   longitude: Number,
-  averageRating: { type: Number, default: 0 }
+  averageRating: { type: Number, default: 0 },
 });
 
-module.exports = mongoose.model("Library", librarySchema);
+module.exports = mongoose.model('Library', librarySchema);
