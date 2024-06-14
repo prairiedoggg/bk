9a81ac94b26e419b1801459c9201c0367cfdb7bc
[AI10] 나경윤 <skruddbs99@naver.com>
2024-06-14 19:54:06 +0900

    fix: App.js 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,9 @@
-// import React from 'react';
-// import './index.css';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import Navbar from './components/common/Navbar';
-// import Intro from './pages/Intro';
-// import Board from './components/board/Board';
-// import Mypage from './pages/Mypage';
-// import Main from './pages/Main';
-// import EditPage from './pages/EditPage';
-// import AddInfo from './pages/AddInfo';
-
-// const App = () => {
-//   return (
-//     <BrowserRouter>
-//       <div className='App'>
-//         <Navbar />
-//         <Routes>
-//           <Route path='/' element={<Intro />} />
-//           <Route path='/library' element={<Main />} />
-//           <Route path='/board' element={<Board />} />
-//           <Route path='/mypage' element={<Mypage />} />
-//           <Route path='/mypage/edit' element={<EditPage />} />
-//           <Route path='/additionalinfo' element={<AddInfo />} />
-//         </Routes>
-//       </div>
-//     </BrowserRouter>
-//   );
-// };
-
-// export default App;
-
 import React from 'react';
 import './index.css';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import Intro from './pages/Intro';
-import Board from './components/board/Board';
+import Board from './components/board/Boards';
 import Mypage from './pages/Mypage';
 import Main from './pages/Main';
 import EditPage from './pages/EditPage';
