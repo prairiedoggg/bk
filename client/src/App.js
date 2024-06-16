dcdbd3dbe8836bebae61bbc818bd6c0e9bcb941e
[AI10] 나경윤 <skruddbs99@naver.com>
2024-06-16 15:59:46 +0900

    fix: 라우터 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import './index.css';
 import {
   BrowserRouter,
   Routes,
   Route,
   useLocation,
-  Navigate
+  Navigate,
+  useNavigate
 } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import Intro from './pages/Intro';
@@ -19,44 +20,38 @@ import { getLoginStatus } from './api/Auth';
 import PostDetails from './pages/PostDetails';
 
 const App = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
   const location = useLocation();
-  const hideNavbarPaths = ['/additionalinfo', '/404'];
+  const navigate = useNavigate();
 
   const checkLoginStatus = async () => {
     try {
       await getLoginStatus();
     } catch (error) {
       console.log('로그인상태', error);
-      setIsLoggedIn(false);
+      if (location.pathname.startsWith('/mypage')) {
+        navigate('/404');
+      }
     }
   };
 
   useEffect(() => {
     checkLoginStatus();
-  }, [location]);
+  }, []);
 
   return (
     <div className='App'>
-      {!hideNavbarPaths.some((path) => location.pathname.startsWith(path)) && (
-        <Navbar />
-      )}
       <Routes>
-        <Route path='/' element={<Intro />} />
-        <Route path='/library' element={<Main />} />
-        <Route path='/board/:shortId' element={<PostDetails />} />
-        <Route path='/board' element={<Board />} />
+        <Route path='/' element={<Navbar />}>
+          <Route index element={<Intro />} />
+          <Route path='library' element={<Main />} />
+          <Route path='board/:shortId' element={<PostDetails />} />
+          <Route path='board' element={<Board />} />
+          <Route path='mypage' element={<Mypage />} />
+          <Route path='mypage/edit' element={<EditPage />} />
+          <Route path='*' element={<Navigate to='/404' />} />
+        </Route>
         <Route path='/additionalinfo' element={<AddInfo />} />
-        {isLoggedIn ? (
-          <>
-            <Route path='/mypage' element={<Mypage />} />
-            <Route path='/mypage/edit' element={<EditPage />} />
-          </>
-        ) : (
-          <Route path='/mypage/*' element={<Navigate to='/404' />} />
-        )}
         <Route path='/404' element={<NotFoundPage />} />
-        <Route path='*' element={<Navigate to='/404' />} />
       </Routes>
     </div>
   );
